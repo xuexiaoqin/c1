294c989a581e83cb3e0a23a21f23e2efa707bef9
jj <xuexiaoqin@qq.com>
2019-01-15 11:40:32 +0800

    构建一个ATM基本框架

--- main.c
+++ main.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    printf("hello world!");
+    printf("进入ATM系统\n");
+    while (1)
+    {
+        printf("1 -- 存钱\n");
+        printf("2 -- 取钱\n");
+        printf("3 -- 余额\n");
+        printf("4 -- 退出\n");
+
+        printf("请输入功能编号\n");
+        int code;
+        scanf("%d",&code);
+        if(code == 1){
+        
+        }
+
+         if(code == 2){
+        
+        }
+         if(code == 3){
+        
+        }
+         if(code == 4){
+        
+        }
+    }
 
     return 0;
-
 }
