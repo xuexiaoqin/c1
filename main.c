465c1c16a80eb81aebe9cb8f240c390f6c543a91
jj <xuexiaoqin@qq.com>
2019-01-15 15:06:28 +0800

    添加一个存钱管理系统，并改正上一个

--- main.c
+++ main.c
@@ -2,34 +2,52 @@
 
 int main()
 {
-
+    int qian = 0;
     printf("进入ATM系统\n");
     while (1)
     {
-        int qian = 0;
+
         printf("1 -- 存钱\n");
         printf("2 -- 取钱\n");
         printf("3 -- 余额\n");
         printf("4 -- 退出\n");
 
         printf("请输入功能编号\n");
-        int code;
+        int code = 0;
         scanf("%d", &code);
         if (code == 1)
         {
-           
+
             int cun;
             printf("进入存钱功能区,请输入存款金额\n");
+
+            scanf("%d", &cun);
             qian += cun;
-            scanf("%d",&cun);
             printf("存钱成功，点击回车继续\n");
-            int x;
-            scanf("%d",&x);
-            scanf("%d",&x);
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
 
         if (code == 2)
         {
+            int qu;
+            char x;
+            printf("进入取钱功能区,请输入取款金额\n");
+            scanf("%d", &qu);
+            if (qian >= qu)
+            {
+                qian -= qu;
+                printf("取钱成功，点击回车继续\n");
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
+            else
+            {
+                printf("余额不足，取款失败，点击回车继续\n");
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
         }
         if (code == 3)
         {
